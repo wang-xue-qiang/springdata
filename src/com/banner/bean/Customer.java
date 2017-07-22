0c0a0b7ee74c7b3c07a903123b9f8c935049d76e
SayHelloAllen <592470261@qq.com>
2017-07-22 15:55:50 +0800

    添加customerDao
    Change-Id: I2d0d0b29c79dd3c5a709863960e9c58a83ffc692
    Signed-off-by: SayHelloAllen <592470261@qq.com>

--- src/com/banner/bean/Customer.java
+++ src/com/banner/bean/Customer.java
@@ -3,6 +3,7 @@ package com.banner.bean;
 public class Customer {
 	public static void main(String[] args) {
 		System.out.println("Hello222222!");
-		
+		int a = 123535;
+		System.out.println(a);
 	}
 }
---
+++ src/com/banner/dao/CustomerDao.java
@@ -0,0 +1,5 @@
+package com.banner.dao;
+
+public class CustomerDao {
+
+}
